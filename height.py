f119855fc8d713c5fc5c6a23232c743ee201c6c5
Mannan Qazi <mannanqazi2@gmail.com>
2018-08-09 14:25:00 -0400

    Height Prediction in Python

---
+++ height.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import sklearn.cross_validation
+
+df = pd.read_csv('height.csv')
+x = df['Weight'].values
+y = df['Height'].values
+x = x.reshape(-1, 1)
+y = y.reshape(-1, 1)
+lm = LinearRegression()
+lm.fit(x,y)
+#print('Int', lm.intercept_)
+#print('Coef:', lm.coef_)
+plt.scatter(y, lm.predict(x))
+plt.xlabel("Actual Height")
+plt.ylabel("Predicted Height")
+plt.title("Actual vs Predicted Height")
+#plt.show()
+lm.predict(x)
+X_train, X_test, Y_train, Y_test = sklearn.cross_validation.train_test_split(x, y, test_size = 0.33, random_state = 5)
+#print(X_train.shape)
+#print(X_test.shape)
+#print(Y_train.shape)
+#print(Y_test.shape)
+
+lm2 = LinearRegression()
+lm2.fit(X_train, Y_train)
+pred_train = lm2.predict(X_train)
+pred_test = lm2.predict(X_test)
+#print("R squared:", lm2.score(X_test, Y_test))
+
+plt.scatter(lm2.predict(X_train), lm2.predict(X_train) - Y_train, c = 'b', alpha = 0.5)
+plt.scatter(lm2.predict(X_test), lm2.predict(X_test) - Y_test, c = 'g')
+plt.hlines(y = 0, xmin = 50, xmax = 80)
+plt.title("Residual Plot using training and test data")
+plt.ylabel("Residuals")
+#plt.show()
+
+#print(type(lm.intercept_))
+#print(type(lm.coef_))
+intercept = lm.intercept_[0]
+
+coef = lm.coef_[[0]]
+
+
+intercept.astype(int)
+coef.astype(int)
+print(type(intercept))
+print(type(coef))
+UserWe = input('Enter Weight:')
+def PredictHeight(UserWe):
+    Height = (float(UserWe) *coef) + intercept
+    #print('Predicted height of the person is:', Height)
+    return Height
+
+print(PredictHeight(UserWe))
